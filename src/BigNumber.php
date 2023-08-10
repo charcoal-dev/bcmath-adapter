f2631db4354bcb8d1092a8154db7ce3ec4028981
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-11 03:48:06 +0400

    BigNumber and tests

---
+++ src/BigNumber.php
@@ -0,0 +1,448 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/bcmath-adapter" package.
+ * https://github.com/charcoal-dev/bcmath-adapter
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/bcmath-adapter/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Adapters\BcMath;
+
+/**
+ * Class BigNumber
+ * @package Charcoal\Adapters\BcMath
+ */
+class BigNumber implements \Stringable
+{
+    /** @var string */
+    private string $value;
+    /** @var int */
+    private int $scale;
+
+    /**
+     * @param string|int|float $value
+     * @param int $scale
+     */
+    public function __construct(string|int|float $value, int $scale = 18)
+    {
+        $this->changeScale($scale);
+        $this->value = bcmul($this->checkValidNum($value), "1", $scale);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return array
+     */
+    public function __debugInfo(): array
+    {
+        return [
+            "value" => $this->value,
+            "scale" => $this->scale
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function __serialize(): array
+    {
+        return [
+            "value" => $this->value,
+            "scale" => $this->scale
+        ];
+    }
+
+    /**
+     * @param array $data
+     */
+    public function __unserialize(array $data): void
+    {
+        $this->value = $this->checkValidNum($data["value"]);
+        $this->changeScale($data["scale"]);
+    }
+
+    /**
+     * @param int $scale
+     * @return $this
+     */
+    public function changeScale(int $scale): self
+    {
+        if ($scale < 0) {
+            throw new \InvalidArgumentException('BcMath scale value must be a positive integer');
+        }
+
+        $this->scale = $scale;
+        return $this;
+    }
+
+    /**
+     * Gets value as string
+     * - If fracDigits argument is > 0, value will be set to N amount of fractional digits after decimal pointer,
+     *  if not enough then further zeros will be added to the value.
+     *    - A value of "0.000000000000000000" (18 scale) will become 0.0000 with retain argument set to 4
+     *    - A value of "0.102300456000000000" will become "0.1023004" if trimToSize is TRUE otherwise "0.102300456"
+     * @param int|null $fracDigits
+     * @param bool $trimToSize
+     * @return string
+     */
+    public function value(?int $fracDigits = null, bool $trimToSize = true): string
+    {
+        if (!$fracDigits || $fracDigits < 0 || $this->isInteger()) {
+            return $this->value;
+        }
+
+        if ($trimToSize) {
+            return bcmul($this->value, "1", $fracDigits);
+        }
+
+        $split = explode(".", rtrim(rtrim($this->value, "0"), "."));
+        $fractional = $split[1] ?? "";
+        $retain = $fracDigits - strlen($fractional);
+        if ($retain > 0) {
+            return $split[0] . "." . $fractional . str_repeat("0", $retain);
+        }
+
+        return $split[0] . "." . $fractional;
+    }
+
+    /**
+     * Retrieves any integer value (that is <= PHP_INT_MAX) as int
+     * @return int
+     */
+    public function int(): int
+    {
+        if (!$this->isInteger()) {
+            throw new \OutOfBoundsException('BigNumber value is not an integer');
+        }
+
+        if (bccomp($this->value, strval(PHP_INT_MAX), 0) === 1) {
+            throw new \OverflowException('BigNumber value exceeds PHP_INT_MAX');
+        }
+
+        return intval($this->value);
+    }
+
+    /**
+     * Checks if value is integral (does not have decimals)
+     * @return bool
+     */
+    public function isInteger(): bool
+    {
+        return (bool)preg_match('/^-?(0|[1-9]+[0-9]*)$/', $this->value);
+    }
+
+    /**
+     * Checks if value is zero
+     * @return bool
+     */
+    public function isZero(): bool
+    {
+        return bccomp($this->value, "0", $this->useScale()) === 0;
+    }
+
+    /**
+     * Checks if value is greater than zero
+     * @return bool
+     */
+    public function isPositive(): bool
+    {
+        return bccomp($this->value, "0", $this->useScale()) === 1;
+    }
+
+    /**
+     * Checks if value is less than zero
+     * @return bool
+     */
+    public function isNegative(): bool
+    {
+        return bccomp($this->value, "0", $this->useScale()) === -1;
+    }
+
+    /**
+     * Compare number with another
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return int
+     */
+    public function cmp(string|float|int|BigNumber $comp, ?int $scale = null): int
+    {
+        $comp = $this->checkValidNum($comp);
+        return bccomp($this->value, $comp, $this->useScale($scale));
+    }
+
+    /**
+     * Compares value with a number to check if both are equal
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return bool
+     */
+    public function equals(string|float|int|BigNumber $comp, ?int $scale = null): bool
+    {
+        return $this->cmp($comp, $scale) === 0;
+    }
+
+    /**
+     * Compares value with a number to check if value is greater than argument
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return bool
+     */
+    public function greaterThan(string|float|int|BigNumber $comp, ?int $scale = null): bool
+    {
+        return $this->cmp($comp, $scale) > 0;
+    }
+
+    /**
+     * Compares value with a number to check if value is greater than or equals argument
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return bool
+     */
+    public function greaterThanOrEquals(string|float|int|BigNumber $comp, ?int $scale = null): bool
+    {
+        return $this->cmp($comp, $scale) >= 0;
+    }
+
+    /**
+     * Compares value with a number to check if value is less than argument
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return bool
+     */
+    public function lessThan(string|float|int|BigNumber $comp, ?int $scale = null): bool
+    {
+        return $this->cmp($comp, $scale) < 0;
+    }
+
+    /**
+     * Compares value with a number to check if value is less than or equals argument
+     * @param string|float|int|BigNumber $comp
+     * @param int|null $scale
+     * @return bool
+     */
+    public function lessThanOrEquals(string|float|int|BigNumber $comp, ?int $scale = null): bool
+    {
+        return $this->cmp($comp, $scale) <= 0;
+    }
+
+    /**
+     * Checks if value is within (or equals) given min and max arguments
+     * @param string|float|int|BigNumber $min
+     * @param string|float|int|BigNumber $max
+     * @param int|null $scale
+     * @return bool
+     */
+    public function inRange(string|float|int|BigNumber $min, string|float|int|BigNumber $max, ?int $scale = null): bool
+    {
+        $scale = $this->useScale($scale);
+        if (bccomp($this->value, $this->checkValidNum($min), $scale) !== -1) {
+            if (bccomp($this->value, $this->checkValidNum($max), $scale) !== 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string|float|int|BigNumber $num
+     * @param int|null $scale
+     * @return $this
+     */
+    public function add(string|float|int|BigNumber $num, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcadd($this->value, $this->checkValidNum($num), $scale), $scale);
+    }
+
+    /**
+     * @param string|float|int|BigNumber $num
+     * @param int|null $scale
+     * @return $this
+     */
+    public function sub(string|float|int|BigNumber $num, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcsub($this->value, $this->checkValidNum($num), $scale), $scale);
+    }
+
+    /**
+     * @param string|float|int|BigNumber $num
+     * @param int|null $scale
+     * @return $this
+     */
+    public function mul(string|float|int|BigNumber $num, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcmul($this->value, $this->checkValidNum($num), $scale), $scale);
+    }
+
+    /**
+     * @param int $base
+     * @param int $exponent
+     * @param int|null $scale
+     * @return $this
+     */
+    public function mulByExp(int $base, int $exponent, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        if ($base < 1) {
+            throw new \InvalidArgumentException('Value for param "base" must be a positive integer');
+        } elseif ($exponent < 1) {
+            throw new \InvalidArgumentException('Value for param "exponent" must be a positive integer');
+        }
+
+        return new static(bcmul($this->value, bcpow(strval($base), strval($exponent), 0), $scale), $scale);
+    }
+
+    /**
+     * @param string|float|int|BigNumber $num
+     * @param int|null $scale
+     * @return $this
+     */
+    public function div(string|float|int|BigNumber $num, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcdiv($this->value, $this->checkValidNum($num), $scale), $scale);
+    }
+
+    /**
+     * @param int $base
+     * @param int $exponent
+     * @param int|null $scale
+     * @return $this
+     */
+    public function divByExp(int $base, int $exponent, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        if ($base < 1) {
+            throw new \InvalidArgumentException('Value for param "base" must be a positive integer');
+        } elseif ($exponent < 1) {
+            throw new \InvalidArgumentException('Value for param "exponent" must be a positive integer');
+        }
+
+        return new static(bcdiv($this->value, bcpow(strval($base), strval($exponent), 0), $scale), $scale);
+    }
+
+    /**
+     * @param string|float|int|BigNumber $num
+     * @param int|null $scale
+     * @return $this
+     */
+    public function pow(string|float|int|BigNumber $num, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcpow($this->value, $this->checkValidNum($num), $scale), $scale);
+    }
+
+    /**
+     * @param string|float|int|BigNumber $divisor
+     * @param int|null $scale
+     * @return $this
+     */
+    public function mod(string|float|int|BigNumber $divisor, ?int $scale = null): static
+    {
+        $scale = $this->useScale($scale);
+        return new static(bcmod($this->value, $this->checkValidNum($divisor), $scale), $scale);
+    }
+
+    /**
+     * @param $divisor
+     * @param int|null $scale
+     * @return $this
+     */
+    public function remainder($divisor, ?int $scale = null): static
+    {
+        return $this->mod($divisor, $scale);
+    }
+
+    /**
+     * @return $this
+     */
+    public function copy(): static
+    {
+        return new static($this->value, $this->scale);
+    }
+
+    /**
+     * @param int|null $scale
+     * @return int
+     */
+    private function useScale(?int $scale = null): int
+    {
+        return is_int($scale) && $scale >= 0 ? $scale : $this->scale;
+    }
+
+    /**
+     * Checks and accepts Integers, Double/Float values or numeric Strings for BcMath operations
+     * @param string|float|int|BigNumber $value
+     * @return string
+     */
+    private function checkValidNum(string|float|int|BigNumber $value): string
+    {
+        $value = static::toString($value);
+        if (is_string($value)) {
+            return $value;
+        }
+
+        throw new \InvalidArgumentException('Invalid argument; Expecting a valid numeric value');
+    }
+
+    /**
+     * Checks if argument is a valid number, returns string if numeric value is valid otherwise NULL.
+     * - Converts integer and floats values to strings to safely worth with Bcmath.
+     * - Expands floats with scientific e-notations.
+     * - Numeric values may be signed (negative values).
+     * @param mixed $value
+     * @return string|null
+     */
+    public static function toString(mixed $value): ?string
+    {
+        if ($value instanceof BigNumber) {
+            return $value->value();
+        }
+
+        // Integers are obviously valid numbers
+        if (is_int($value)) {
+            return strval($value);
+        }
+
+        // Floats must be checked for scientific E-notations
+        if (is_float($value)) {
+            $floatAsString = strval($value);
+            // Look if scientific E-notation
+            if (preg_match('/e-/i', $floatAsString)) {
+                // Auto-detect decimals
+                $decimals = preg_split('/e-/i', $floatAsString);
+                $decimals = strlen($decimals[0]) + intval($decimals[1]);
+                return rtrim(number_format($value, $decimals, ".", ""), "0");
+            } elseif (preg_match('/e\+?/i', $floatAsString)) {
+                return number_format($value, 0, "", "");
+            }
+
+            return $floatAsString;
+        }
+
+        // Check with in String
+        if (is_string($value)) {
+            if (preg_match('/^-?(0|[1-9]+[0-9]*)(\.[0-9]+)?$/', $value)) {
+                return $value;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/BigNumberTest.php
@@ -0,0 +1,241 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/bcmath-adapter" package.
+ * https://github.com/charcoal-dev/bcmath-adapter
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/bcmath-adapter/blob/master/LICENSE
+ */
+
+use Charcoal\Adapters\BcMath\BigNumber;
+
+/**
+ * Class BigNumberTest
+ */
+class BigNumberTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Testing behaviour when object of BigNumber is directly treated as a string value
+     * @return void
+     */
+    public function testAsString(): void
+    {
+        $bN = new BigNumber(1.23456789, 4);
+        $this->assertEquals("1.2345", $bN);
+        $this->assertEquals(6, strlen($bN));
+    }
+
+    /**
+     * Testing "isPositive", "isNegative" and "isZero" methods.
+     * @return void
+     */
+    public function testStates(): void
+    {
+        // Scale of 18
+        $this->assertTrue((new BigNumber("0.000000000000000001", 18))->isPositive());
+        // last digit is discarded due to scale of 17:
+        $this->assertFalse((new BigNumber("0.000000000000000001", 17))->isPositive());
+
+        $this->assertTrue((new BigNumber("0.000000000000000000", 18))->isZero());
+        // last digit is actually 19th fractional part:
+        $this->assertTrue((new BigNumber("0.0000000000000000001", 18))->isZero());
+        $this->assertTrue((new BigNumber("0", 18))->isZero());
+        $this->assertTrue((new BigNumber(0, 0))->isZero());
+
+        $this->assertTrue((new BigNumber("-1", 0))->isNegative());
+        $this->assertTrue((new BigNumber(-0.000001, 6))->isNegative());
+    }
+
+    /**
+     * Testing "isInteger" and "int" value methods
+     * @return void
+     */
+    public function testIntegers(): void
+    {
+        // Simple tests
+        $this->assertFalse((new BigNumber("0.000"))->isInteger());
+        $this->assertFalse((new BigNumber(0.001))->isInteger());
+        $this->assertFalse((new BigNumber("1.20304"))->isInteger());
+
+        // Following tests demonstrate if scale value > 0 for visibly int values,
+        // constructor will automatically append N amount of fractional parts...
+        $this->assertFalse((new BigNumber(1))->isInteger()); // No longer an int
+        $this->assertTrue((new BigNumber(1, 0))->isInteger()); // With scale 0, it is an int
+        $this->assertFalse((new BigNumber("1"))->isInteger());
+        $this->assertTrue((new BigNumber("1", 0))->isInteger());
+        $this->assertTrue((new BigNumber(0xffff, scale: 0))->isInteger()); // 65535
+        $this->assertEquals("1.0000", (new BigNumber(1, 4))->value()); // int(1) with scale 4 becomes "1.0000" internally
+        $this->assertEquals("65535.00", (new BigNumber(0xffff, 2))->value());
+
+        // Get value as integer
+        $this->assertEquals(250, (new BigNumber(0xff - 5, scale: 0))->int());
+        $this->assertEquals("250", (new BigNumber(0xff - 5, scale: 0))->int());
+
+        // Test maximum value as int
+        $this->assertEquals(9223372036854775807, (new BigNumber(PHP_INT_MAX, scale: 0))->int());
+    }
+
+    /**
+     * Attempt to retrieve a decimal value as int
+     * @return void
+     */
+    public function testDecimalValueAsInt(): void
+    {
+        $this->expectException('OutOfBoundsException');
+        (new BigNumber(1.23456))->int();
+    }
+
+    /**
+     * A very big integer value that exceeds PHP_INT_MAX
+     * @return void
+     */
+    public function testOverflowAsInt(): void
+    {
+        $bN = (new BigNumber(PHP_INT_MAX, 0))->add(1); // Simply add 1
+        $this->assertEquals("9223372036854775808", $bN->value());
+        $this->expectException('OverflowException');
+        $bN->int();
+    }
+
+    /**
+     * Test scaling values provided to constructor and later to arithmetic ops
+     * @return void
+     */
+    public function testScaling(): void
+    {
+        $bN = new BigNumber(0, 18);
+        $this->assertEquals("0.000000000000000000", $bN->value());
+        $bN = $bN->add("0.12345678");
+        $this->assertEquals("0.123456780000000000", $bN->value());
+        $bN = $bN->mul(2);
+        $this->assertEquals("0.246913560000000000", $bN->value());
+        $bN = $bN->mul(4, scale: 3);
+        $this->assertEquals("0.987", $bN->value());
+        $this->assertEquals("0.98701", $bN->add("0.000012345", scale: 5)->value());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSerialization(): void
+    {
+        $bn1 = new BigNumber(1.2345678, 4);
+        $bn1s = serialize($bn1);
+        $bn1b = unserialize($bn1s);
+        $this->assertTrue($bn1->equals($bn1b));
+
+        /** @var BigNumber $bn2 */
+        $bn2 = unserialize(serialize((new BigNumber("-1.03", 4))));
+        $this->assertEquals("-1.0300", $bn2->value());
+
+        /** @var BigNumber $bn3 */
+        $bn3 = unserialize(serialize((new BigNumber("0.000000000000000138", scale: 18))));
+        $this->assertEquals("0.000000000000000138", $bn3->value());
+    }
+
+    /**
+     * Test the static method BigNumber::toString()
+     * @return void
+     */
+    public function testBigNumber2String(): void
+    {
+        $this->assertEquals("0", BigNumber::toString(0.00));
+        $this->assertEquals("0", (new BigNumber(0.00, scale: 0))->value());
+        $this->assertEquals("0.00", (new BigNumber(0.0, scale: 2))->value());
+
+        $this->assertEquals("0.00000001", BigNumber::toString(0.00000001));
+        $this->assertEquals("0.00000001", BigNumber::toString("0.00000001"));
+        $this->assertEquals("0.00000001", BigNumber::toString(1e-8));
+        $this->assertEquals("0.00000233", BigNumber::toString(0.233e-5));
+
+        $this->assertEquals("3000000", BigNumber::toString(0.3e+7));
+        $this->assertEquals("3000000", BigNumber::toString(3e+6));
+        $this->assertEquals("3430000000", BigNumber::toString(3.43e+9));
+        $this->assertEquals("1.61803398875", BigNumber::toString(1.61803398875e+0));
+
+        $this->assertEquals("12", BigNumber::toString(1.2e+1));
+        $this->assertEquals("12", BigNumber::toString(1.2e1));
+        $this->assertEquals("0.12", BigNumber::toString(12e-2));
+
+        $this->assertEquals("191919", BigNumber::toString(new BigNumber(191919, scale: 0)));
+        $this->assertEquals("19.919", BigNumber::toString(new BigNumber(19.919, scale: 3)));
+    }
+
+    /**
+     * Test comparison operators
+     * @return void
+     */
+    public function testCompares(): void
+    {
+        $bN1 = new BigNumber("1.0034");
+        $this->assertTrue($bN1->equals("1.0034"));
+        $this->assertTrue($bN1->greaterThanOrEquals("1.0034"));
+        $this->assertTrue($bN1->lessThanOrEquals("1.0034"));
+
+        $this->assertFalse($bN1->greaterThan("1.0034000000001"));
+        $this->assertTrue($bN1->greaterThan("1.00339"));
+
+        $this->assertTrue($bN1->lessThan("1.00340000001"));
+        $this->assertTrue($bN1->lessThan("1.0035"));
+    }
+
+    /**
+     * Test simple arithmetic operations
+     * @return void
+     */
+    public function testArithmeticOps(): void
+    {
+        $bN1 = new BigNumber(PHP_INT_MAX, scale: 4);
+        $bN2 = $bN1->add("3.001234");
+        $this->assertEquals("9223372036854775810.0012", $bN2->value());
+        $bN3 = $bN2->mul(3.5);
+        $this->assertEquals("32281802128991715335.0042", $bN3->value());
+        $bN4 = $bN3->sub("32281802128991715335", scale: 6);
+        $this->assertEquals("0.004200", $bN4->value());
+
+        $bN5 = (new BigNumber(5.34))->divByExp(10, 8);
+        $this->assertEquals("0.000000053400000000", $bN5->value());
+        $bN6 = (new BigNumber(25351, scale: 0))->divByExp(10, 8, scale: 12);
+        $this->assertEquals("0.000253510000", $bN6->value());
+        $this->assertEquals("0.00025351", $bN6->value(8));
+
+        $bN7 = (new BigNumber("3.00304567", scale: 8))->mulByExp(10, 8, scale: 0);
+        $this->assertEquals(300304567, $bN7->int());
+
+        $bN8 = $bN7->div(2.22, scale: 4);
+        $this->assertEquals("135272327.4774", $bN8->value());
+    }
+
+    /**
+     * Test optional trimming of fractional digits for "value" method
+     * @return void
+     */
+    public function testOutputs(): void
+    {
+        $bN = new BigNumber(1.2345678, scale: 4);
+        $this->assertEquals("1.2345", $bN->value());
+
+        $bN1 = new BigNumber(0, scale: 18);
+        $this->assertEquals("0.000000", $bN1->value(6));
+        $this->assertEquals("0.000000000000000000", $bN1->value());
+
+        $bN2 = new BigNumber(0.12345678, scale: 18);
+        $this->assertEquals("0.1234", $bN2->value(4));
+        $this->assertEquals("0.12", $bN2->value(2));
+        $this->assertEquals("0.123456", $bN2->value(6));
+
+        $bN3 = new BigNumber(0.102300456, scale: 18);
+        $this->assertEquals("0.102300456000000000", $bN3->value());
+        $this->assertEquals("0.1023004", $bN3->value(fracDigits: 7));
+        $this->assertEquals("0.102300456", $bN3->value(fracDigits: 7, trimToSize: false));
+        $this->assertEquals("0.102300456000", $bN3->value(fracDigits: 12));
+        $this->assertEquals("0.102300456000", $bN3->value(fracDigits: 12, trimToSize: false));
+
+        $bN4 = new BigNumber("0.103", scale: 18);
+        $this->assertEquals("0.103000", $bN4->value(6));
+        $this->assertEquals("0.103000", $bN4->value(6, false));
+    }
+}
